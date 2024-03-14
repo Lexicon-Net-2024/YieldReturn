80453c2f1676493f20a3f342086389da0e88bc23
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-14 13:55:55 +0100

    Some demo code

--- YieldReturn/Program.cs
+++ YieldReturn/Program.cs
@@ -5,6 +5,44 @@
         static void Main(string[] args)
         {
             var numbers = GetNumbers();
+            IEnumerator<int> enumerator = numbers.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                int item = enumerator.Current;
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in GetNumersYield().Take(10))
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        private static IEnumerable<int> GetNumersYield()
+        {
+            int i = 0;
+
+            while (true)
+            {
+                yield return i++;
+            }
+        }
+
+        private static IEnumerable<int> GetNumersOrdinary()
+        {
+            int i = 0;
+            var res = new List<int>();
+
+            while(i < 100000000)
+            {
+                res.Add(i++);
+            }
+
+            return res;
+
         }
 
         private static IEnumerable<int> GetNumbers()
