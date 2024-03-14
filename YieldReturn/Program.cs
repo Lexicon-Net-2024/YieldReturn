797087ffd49a6072adbccbb2620aedc6f2348f64
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-14 12:59:26 +0100

    Add project files.

---
+++ YieldReturn/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace YieldReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = GetNumbers();
+        }
+
+        private static IEnumerable<int> GetNumbers()
+        {
+            var num = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+            foreach (var number in num)
+            {
+                yield return number;
+            }
+        }
+    }
+}
